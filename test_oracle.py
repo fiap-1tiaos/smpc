90d1949a81bed28e3bd51fa22be5ffc6bca897d7
Gabriel Oliveira <oldsgabriel@gmail.com>
2025-10-12 12:32:43 -0300

    removendo arquivo de teste com banco

--- test_oracle.py
+++
@@ -1,9 +0,0 @@
-import os
-
-instant_client_path = r"C:\Users\defaultuser0\Downloads\sqldeveloper\oracledb_client\instantclient_23_9"
-os.environ["PATH"] = instant_client_path + ";" + os.environ["PATH"]
-
-os.add_dll_directory(instant_client_path)
-
-import cx_Oracle
-print("Versão do Oracle Client:", cx_Oracle.clientversion())
